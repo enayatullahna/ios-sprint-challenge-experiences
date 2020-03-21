436dd42a2cf7f0f50da9d0dee30f223a0ab8bbec
Enayatullah Naseri <enayatullahna@gmail.com>
2020-03-21 10:22:30 -0700

    Updated UI

---
+++ Experiences/Experiences/View Controllers/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  Experiences
+//
+//  Created by Enayatullah Naseri on 3/21/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
